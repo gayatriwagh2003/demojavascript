6920a04fe1b6fc2ec757913376f9bfaea7a6d65b
gayatriwagh2003 <139702601+gayatriwagh2003@users.noreply.github.com>
2024-11-16 09:53:21 +0530

    Update demo.js

--- demojavascript/demo.js
+++ demojavascript/demo.js
@@ -1,1 +1,20 @@
-console.log("hello");
+<Html>
+
+<body>
+git clone https://gitlab.com/kkw3431946/MyProject.git
+cd Myproject
+git status
+File1.txt
+git add .
+git commit -m "First change"
+git branch newbranch
+git status
+git add .
+git commit -m "second change"
+git push origin newbranch
+git checkout main
+git pull origin main
+git log
+</body>
+  
+</html>
